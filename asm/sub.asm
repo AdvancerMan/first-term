46c5ebc2d2c82f20461c307f326e459f2d4b6cd2
Kazakov Mihail <advancerman@gmail.com>
2020-06-29 19:53:17 +0700

    Added asm hw

--- asm/sub.asm
+++ asm/sub.asm
@@ -3,16 +3,18 @@
                 global          _start
                 extern          read_long, write_long, write_char
 _start:
-
-                sub             rsp, 2 * 128 * 8
-                lea             rdi, [rsp + 128 * 8]
-                mov             rcx, 128
+                
+                sub             rsp, 2 * long_integer_size * 8
+                lea             rdi, [rsp + long_integer_size * 8]
+                mov             rcx, long_integer_size
                 call            read_long
                 mov             rdi, rsp
                 call            read_long
-                lea             rsi, [rsp + 128 * 8]
-                call            add_long_long
-
+                
+                mov             rsi, rsp
+                lea             rdi, [rsp + long_integer_size * 8]
+                call            sub_long_long
+                
                 call            write_long
 
                 mov             al, 0x0a
@@ -20,13 +22,13 @@ _start:
 
                 jmp             exit
 
-; adds two long number
-;    rdi -- address of summand #1 (long number)
-;    rsi -- address of summand #2 (long number)
+; subtracts one unsigned long number from another unsigned long number
+;    rdi -- address of minuend (unsigned long number)
+;    rsi -- address of subtrahend (unsigned long number)
 ;    rcx -- length of long numbers in qwords
 ; result:
-;    sum is written to rdi
-add_long_long:
+;    result is written to rdi
+sub_long_long:
                 push            rdi
                 push            rsi
                 push            rcx
@@ -35,7 +37,7 @@ add_long_long:
 .loop:
                 mov             rax, [rsi]
                 lea             rsi, [rsi + 8]
-                adc             [rdi], rax
+                sbb             [rdi], rax
                 lea             rdi, [rdi + 8]
                 dec             rcx
                 jnz             .loop
@@ -49,4 +51,7 @@ exit:
                 mov             rax, 60
                 xor             rdi, rdi
                 syscall
+
+                section         .rodata
+long_integer_size: equ          128
 
