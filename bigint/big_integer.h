7e805ee8e852d895f00fc3397bfe8a0010b29c09
Kazakov Mihail <advancerman@gmail.com>
2020-07-01 14:34:10 +0700

    Added bigint hw

--- bigint/big_integer.h
+++ bigint/big_integer.h
@@ -1,17 +1,24 @@
-#ifndef BIG_INTEGER_H
-#define BIG_INTEGER_H
+#pragma once
 
 #include <cstddef>
-#include <gmp.h>
 #include <iosfwd>
+#include <stdint.h>
+#include <utility>
+#include <vector>
 
-struct big_integer
-{
+struct big_integer {
+    using int_t = uint32_t;
+    using double_int_t = uint64_t;
+    using quad_int_t = __uint128_t;
+    static const int INT_T_BITS = 32;
+    static const int_t INT_T_MAX = static_cast<int_t>(-1);
+    using storage_t = std::vector<int_t>;
+
     big_integer();
     big_integer(big_integer const& other);
     big_integer(int a);
     explicit big_integer(std::string const& str);
-    ~big_integer();
+    ~big_integer() = default;
 
     big_integer& operator=(big_integer const& other);
 
@@ -38,18 +45,39 @@ struct big_integer
     big_integer& operator--();
     big_integer operator--(int);
 
-    friend bool operator==(big_integer const& a, big_integer const& b);
-    friend bool operator!=(big_integer const& a, big_integer const& b);
-    friend bool operator<(big_integer const& a, big_integer const& b);
-    friend bool operator>(big_integer const& a, big_integer const& b);
-    friend bool operator<=(big_integer const& a, big_integer const& b);
-    friend bool operator>=(big_integer const& a, big_integer const& b);
+    friend std::ostream& operator<<(std::ostream& s, big_integer a);
 
-    friend std::string to_string(big_integer const& a);
+    int compare_to(big_integer const&) const;
+    big_integer& negate();
+    void swap(big_integer&);
+    big_integer& negate_bits();
+    bool is_negative() const;
+    std::tuple<big_integer, big_integer> divide(big_integer);
 
 private:
-    mpz_t mpz;
+    big_integer(int_t);
+    int_t get(size_t) const;
+    int_t get_rest() const;
+    size_t size() const;
+    big_integer& push_zero();
+    big_integer& bit_operation(int_t (int_t, int_t), big_integer const&);
+    void shrink_to_fit();
+    std::tuple<big_integer, int_t> divide(int_t rhs);
+    std::tuple<big_integer, big_integer> long_divide(big_integer const& rhs);
+    std::tuple<big_integer, big_integer> divide_positive(big_integer const&);
+    friend int_t trial(big_integer const&, big_integer const&, size_t);
+
+    storage_t values;
 };
+
+bool operator==(big_integer const& a, big_integer const& b);
+bool operator!=(big_integer const& a, big_integer const& b);
+bool operator<(big_integer const& a, big_integer const& b);
+bool operator>(big_integer const& a, big_integer const& b);
+bool operator<=(big_integer const& a, big_integer const& b);
+bool operator>=(big_integer const& a, big_integer const& b);
+
+std::string to_string(big_integer const& a);
 
 big_integer operator+(big_integer a, big_integer const& b);
 big_integer operator-(big_integer a, big_integer const& b);
@@ -63,15 +91,3 @@ big_integer operator^(big_integer a, big_integer const& b);
 
 big_integer operator<<(big_integer a, int b);
 big_integer operator>>(big_integer a, int b);
-
-bool operator==(big_integer const& a, big_integer const& b);
-bool operator!=(big_integer const& a, big_integer const& b);
-bool operator<(big_integer const& a, big_integer const& b);
-bool operator>(big_integer const& a, big_integer const& b);
-bool operator<=(big_integer const& a, big_integer const& b);
-bool operator>=(big_integer const& a, big_integer const& b);
-
-std::string to_string(big_integer const& a);
-std::ostream& operator<<(std::ostream& s, big_integer const& a);
-
-#endif // BIG_INTEGER_H
